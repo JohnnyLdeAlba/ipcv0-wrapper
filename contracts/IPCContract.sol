a2e379002caba8941c156e2c0f81e5785a999d33
Johnny L. de Alba <johnnyldealba@gmail.com>
2022-08-02 06:10:56 -0700

    Second commit.

--- contracts/IPCContract.sol
+++ contracts/IPCContract.sol
@@ -15,8 +15,9 @@ interface IPCCore {
     uint128 experience,
     uint128 timeOfBirth);
 
+  function ownerOf(uint256 tokenId) external view returns (address);
   function totalSupply() external view returns (uint256);
-  function setIpcPrice(uint ipcId, uint newPrice) external payable;
+  function setIpcPrice(uint tokenId, uint newPrice) external payable;
   function safeTransferFrom(
     address from,
     address to,
@@ -34,6 +35,9 @@ contract IPCV0Wrapper is IERC721Receiver, Ownable, ERC721A {
   address public immutable contractAddress;
 
   // set token limit...
+  string _tokenURI;
+  string _contractURI;
+
   uint256 tokenLimit;
   mapping(uint256 => t_token) tokenList;
   mapping(uint256 => uint256) tokenIndexList;
@@ -47,8 +51,11 @@ contract IPCV0Wrapper is IERC721Receiver, Ownable, ERC721A {
   error TokenNotOwner();
 
   constructor() ERC721A("Immortal Player Characters v0", "IPCV0") {
-  
+
     contractAddress = address(0);
+    _tokenURI = "";
+    _contractURI = "";
+
     tokenLimit = 1000;
   }
 
@@ -115,6 +122,66 @@ contract IPCV0Wrapper is IERC721Receiver, Ownable, ERC721A {
   function _startTokenId()
     internal view virtual override
     returns (uint256) { return 1; }
+
+  function _baseURI() internal view override returns (string memory) {
+    return _tokenURI;
+  }
+
+  function setTokenURI(string calldata __tokenURI) public onlyOwner {
+    _tokenURI = __tokenURI;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public view override returns (string memory) {
+
+      if (tokenId > tokenLimit || tokenId == 0)
+        revert URIQueryForNonexistentToken();
+
+      string memory baseURI = _baseURI();
+      return bytes(baseURI).length != 0 ? string(abi.encodePacked(baseURI, _toString(tokenId))) : '';
+  }
+
+  function setContractURI(string calldata __contractURI) public onlyOwner {
+     _contractURI = __contractURI;
+  }
+
+  function contractURI() public view returns (string memory) {
+    return _contractURI;
+  }
+
+  function getIpc(uint256 tokenId)
+    external view returns (
+      string memory name,
+      bytes32 attributeSeed,
+      bytes32 dna,
+      uint128 experience,
+      uint128 timeOfBirth
+    ) {
+      return IPCCore(contractAddress).getIpc(tokenId);
+  }
+
+  function getAllTokens(uint256 startIndex, uint256 total)
+    public view returns(memory) {
+
+      uint256 totalTokens = totalSupply();
+  }
+
+  function ownerOf(uint256 tokenId)
+    public view override returns (address) {
+
+      uint256 tokenIndex = tokenIndexList[tokenId];
+      if (tokenIndex == 0) {
+ 
+        return IPCCore(contractAddress).ownerOf(tokenId); 
+      }
+
+      address owner = tokenList[tokenIndex].owner;
+      return owner;
+  }
+
+  function totalSupply() public view override returns (uint256) {
+    return IPCCore(contractAddress).totalSupply();
+  }
 
   function onERC721Received(
     address,
