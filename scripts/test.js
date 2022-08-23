624d0598e5c8f6a1b9a605774af3aa08bcd0770b
Johnny L. de Alba <johnnyldealba@gmail.com>
2022-08-23 11:52:15 -0700

    Contract fully tested.

--- scripts/test.js
+++ scripts/test.js
@@ -9,6 +9,8 @@ const oldAddress = "0x1B171C2E72f529377949a5B597E93DC14Da586A7";
 // deployer: approve, wrap, transfer to account1, deployer: setPrice, buy, account1: unwrap
 // deployer: x5 wrap, xfer to account1, account1: x5 unwrap
 // account1 x5 approve, wrap, unwrap
+// getOwnersTokens, uwGetOwnersTokens, wGetOwnersTokenIds, uwGetOwnersTokenIds, getAllTokens
+// admin functions tested (everything getProperties returns
 
 async function main() {
 
@@ -35,18 +37,29 @@ async function main() {
   console.log("Before Source ownerOf: " + message);
 
 
-  let index = 0;
-  for (index = 0; index < 5; index++) {
+  // let index = 0;
+  // for (index = 0; index < 20; index++) {
 
-    // await oldContract.connect(account1).approve(wrapAddress, tokenId + index);
-    // await contract.connect(account1).unwrap(tokenId + index);
-    // await contract.connect(deployer)["safeTransferFrom(address,address,uint256)"](deployer.address, account1.address, tokenId + index);
-  }
+    // await oldContract.connect(deployer).approve(wrapAddress, tokenId + index);
+    // await contract.connect(deployer).wrap(tokenId + index);
+    // await contract.connect(account1).approve(deployer.address, tokenId + index);
+    // await contract.connect(deployer)["safeTransferFrom(address,address,uint256)"](account1.address, deployer.address, tokenId + index);
+  // }
 
    // await contract.connect(account1)["safeTransferFrom(address,address,uint256)"](account1.address, deployer.address, tokenId);
 
   // await oldContract.connect(deployer).approve(wrapAddress, tokenId);
   // await contract.connect(deployer).wrap(tokenId);
+  // await contract.connect(deployer).changeIpcName(tokenId, "Grumble");
+
+  message = []
+  message[0] = await contract.connect(deployer).totalSupply();
+  message[1] = await contract.connect(deployer).uwBalanceOf(deployer.address);
+  message[2] = await contract.connect(deployer).ownerOf(tokenId);
+  message[3] = await contract.connect(deployer).uwOwnerOf(tokenId);
+  message[4] = await contract.connect(deployer).wOwnerOf(tokenId);
+
+  console.log(message);
 
   // await oldContract.connect(deployer).setIpcPrice(tokenId, 0);
   // await oldContract.connect(deployer).buyIpc(tokenId, 0);
@@ -57,8 +70,18 @@ async function main() {
   // await contract.connect(account1).unwrap(tokenId);
 
 
-  message = await contract.connect(account1).getTokensOfOwner(account1.address, 0, 100);
-  console.log(message);
+  // message = await contract.connect(deployer).uwGetAllTokens(10, 40);
+  // console.log(message);
+
+
+  // await contract.connect(deployer).setContractAddress("0x1B171C2E72f529377949a5B597E93DC14Da586A7");
+  // await contract.connect(deployer).setTokenomics(666, 666, true);
+  // await contract.connect(deployer).setMetaDataURIs("https://pornhub.com/", "https://ixxx.com/contracturi");
+
+  // message = await contract.connect(deployer).contractURI();
+  // console.log("Returned: " + message);
+  // message = await contract.connect(deployer).tokenURI(20);
+  // console.log("Returned: " + message);
 
   // message = await contract.connect(deployer).getProperties();
   // console.log("Returned: " + message);
@@ -100,6 +123,7 @@ async function main() {
   console.log("Difference after:: " + (before - after));
   console.log("Difference after: " + ethers.utils.formatEther((before - after) + ""));
   console.log("Difference after: " + ethers.utils.formatEther((after - before) + ""));
+  
 }
 
 main()
